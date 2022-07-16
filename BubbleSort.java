fd19e47f1c7a1d5dc62a43e1b705901d5a2d1e13
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2022-07-17 00:05:53 +0300

    pyah

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,12 +5,12 @@ public class BubbleSort implements Sort{
     public int[] sort(int[] myArray) {
         int counter;
         do {
-            counter = 0;
+            counter = 0; //обнуляем каунтер
             for (int i = 0; i < myArray.length - 1; i++) {
                 if (myArray[i] > myArray[i + 1]) { //сравниваем рядом стоящие значения
-                    int a = myArray[i + 1]; // вводим буферную переменную и присваиваем ей меньшее значение
+                    int temp = myArray[i + 1]; // вводим буферную переменную и присваиваем ей меньшее значение
                     myArray[i + 1] = myArray[i]; //второму числу присваиваем большее значение
-                    myArray[i] = a; //первому числу присваиваем меньшее значение
+                    myArray[i] = temp; //первому числу присваиваем меньшее значение
                     ++counter;//если не 0, то цикл повторяется.
                 }
             }
